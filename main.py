23b018947089f57fcd5627f6d075c74600ae9b09
cerickson21 <45769406+cerickson21@users.noreply.github.com>
2020-06-23 14:12:58 -0600

    Making a kv file

--- main.py
+++ main.py
@@ -3,37 +3,22 @@ from kivy.app import App
 from kivy.uix.camera import Camera
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.lang import Builder
 
+kv = Builder.load_file("screens.kv")
+
+class PlateCamera(BoxLayout):
+    # Take the current frame of the video as the photo graph
+    def onCameraClick(self, *args):
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        camera.export_to_png("PlatePhotos/IMG_{}.png".format(timestr))
 
 class CameraExample(App):
 
     def build(self):
-        layout = BoxLayout(orientation='vertical')
-
-        # Create a camera object
-        self.cameraObject = Camera(play=False)
-        self.cameraObject.play = True
-        self.cameraObject.resolution = (300,300)  # Specify the resolution
-
-        # Create a button for taking photograph
-        self.camaraClick = Button(text="Take Photo")
-        self.camaraClick.size_hint = (.5, .2)
-        self.camaraClick.pos_hint = {'x': .25, 'y': .75}
-
-        # bind the button's on_press to onCameraClick
-        self.camaraClick.bind(on_press=self.onCameraClick)
-
-        # add camera and button to the layout
-        layout.add_widget(self.cameraObject)
-        layout.add_widget(self.camaraClick)
-
         # return the root widget
-        return layout
-
-    # Take the current frame of the video as the photo graph
-    def onCameraClick(self, *args):
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        self.cameraObject.export_to_png("PlatePhotos/IMG_{}.png".format(timestr))
+        return PlateCamera()
 
 
 # Start the Camera App
