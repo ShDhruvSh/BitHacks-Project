a1441d7a335da7d9acbe0d82581d21c010eed7fd
cerickson21 <45769406+cerickson21@users.noreply.github.com>
2020-06-23 18:34:12 -0600

    Picture Confirmation Screen
    Test if it works plz

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
 import time
 reportCount = 0
-
+imageName = ""
 
 class CameraWindow(Screen):
     def onCameraClick(self, *args):
         camera = self.ids['camera']
         timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("PlatePhotos/IMG_{}.png".format(timestr))
+        imageName = "IMG_"+timestr+".png"
+        camera.export_to_png(imageName)
+    pass
+
+
+class ConfirmPhotoWindow(Screen):
+    def getPhoto(self):
+        plate_image = Image(source = 'PlatePhotos/'+imageName, size_hint = (1, 0.8), pos_hint = {'x': 0, 'y': 0.2})
+
     pass
 
 
@@ -48,6 +57,7 @@ kv = Builder.load_file("screens.kv")
 class CopReporter(App):
     def build(self):
         return kv
+
 
 
 if __name__ == "__main__":
