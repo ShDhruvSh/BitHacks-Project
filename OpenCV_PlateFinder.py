6b6d8e6be5858243f3e1fe30c18ec24e38854b3b
Emily Amspoker <eamspoker21@kentdenver.org>
2020-06-25 15:19:09 -0600

    gets the individual digits, but not in the right order

--- OpenCV_PlateFinder.py
+++ OpenCV_PlateFinder.py
@@ -1,3 +1,6 @@
+from imutils.perspective import four_point_transform
+from imutils import contours
+import imutils
 from PIL import Image
 import cv2
 import numpy as np
@@ -6,6 +9,8 @@ from utils import detect_lp
 from os.path import splitext, basename
 from keras.models import model_from_json
 import glob
+
+%matplotlib inline
 
 
 def load_model(path):
@@ -40,7 +45,49 @@ def get_plate(image_path):
     side = int(ratio * Dmin)
     bound_dim = min(side, Dmax)
     _ , LpImg, _, cor = detect_lp(wpod_net, vehicle, bound_dim, lp_threshold=0.5)
-    plt.imsave("scans/cropped.jpg", img)
+    count = 0
+    for img in LpImg:
+        plt.imsave("scans/cropped" + str(count) + ".jpg", img)
+        count += 1
     return LpImg
 
-img = get_plate("PhotosToClean/download-11.jpg")[0]
+img = get_plate("PhotosToClean/download-4.jpg")[0]
+
+def get_digits(image_path, lp_num):
+    cropped = cv2.imread(image_path, cv2.CV_8UC1)
+    image = imutils.resize(cropped, height=500)
+    # operations to cleanup the thresholded image
+    thresh = cv2.threshold(image, 0, 255,
+    cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    # find contours in the thresholded image, then initialize the
+    # digit contours lists
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    digitCnts = []
+    # loop over the digit area candidates
+    count = 0
+    for c in cnts:
+        # compute the bounding box of the contour
+        (x, y, w, h) = cv2.boundingRect(c)
+        # if the contour is sufficiently large, it must be a digit
+        if (w <= 400)and (h >= 100):
+            digitCnts.append(c)
+            digit = image[y:y+h, x:x+w]
+            plt.imsave("scans/digit"+ str(lp_num) + "-" + str(count)+".jpg", digit)
+            count += 1
+        cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 2)
+
+# scan each plate in the image and get the digits
+def scan_plate(image_path):
+    lpImg = get_plate(image_path)
+    count = 0
+    for image in lpImg:
+        path = "scans/cropped" + str(count) + ".jpg"
+        get_digits(path, count)
+        count += 1
+
+scan_plate("PhotosToClean/download-4.jpg")
+        
