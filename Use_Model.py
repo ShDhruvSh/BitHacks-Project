d0de1d252e52e10a60bf8f8b5d2985706edd0bfa
JWriter20 <46006813+JWriter20@users.noreply.github.com>
2020-06-25 20:59:22 -0600

    Add files via upload

--- Use_Model.py
+++ Use_Model.py
@@ -7,55 +7,76 @@ import matplotlib.pyplot as plt
 import os
 from PIL import Image
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+class predictionMethods():
+    def returnPrediction(self, imgFile):
 
-model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=784))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(36, activation='softmax'))
-model.load_weights('model.h5')
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-def returnPrediction(imgFile):
-    imgSize = 28
+        model = Sequential()
+        model.add(Dense(64, activation='relu', input_dim=784))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(36, activation='softmax'))
+        model.load_weights('model.h5')
 
-    imgPath = '/Users/jakewriter/PycharmProjects/BitHacks-project/scans/' + imgFile
+        imgSize = 28
 
-    img_array = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE)
-    resizedArray = cv2.resize(img_array, (imgSize, imgSize))
-    thresh = 120
-    BWImage = cv2.threshold(resizedArray, thresh, 255, cv2.THRESH_BINARY)[1]
+        imgPath = '/Users/jakewriter/PycharmProjects/BitHacks-project/scans/' + imgFile
 
-    numBlack = 0
-    numWhite = 0
-    for pix in np.nditer(BWImage):
-        if pix == 255:
-            numWhite += 1
+        img_array = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE)
+        resizedArray = cv2.resize(img_array, (imgSize, imgSize))
+        thresh = 120
+        BWImage = cv2.threshold(resizedArray, thresh, 255, cv2.THRESH_BINARY)[1]
+
+        '''
+        numBlack = 0
+        numWhite = 0
+        for pix in np.nditer(BWImage):
+            if pix == 255:
+                numWhite += 1
+            else:
+                numBlack += 1
+        if numWhite < numBlack:
+            BWImage = cv2.bitwise_not(BWImage)
+        '''
+
+        #plt.imshow(BWImage)
+        #plt.show()
+
+        BWImage = np.array(BWImage).reshape(-1, 784)
+
+
+
+        predictions = model.predict(BWImage)
+        prediction = np.argmax(predictions, axis=1)
+
+        if prediction > 9:
+            prediction = str(chr(prediction + 55))
         else:
-            numBlack += 1
-    if numWhite < numBlack:
-        BWImage = cv2.bitwise_not(BWImage)
-
-    plt.imshow(BWImage)
-    plt.show()
-
-    BWImage = np.array(BWImage).reshape(-1, 784)
+            prediction = str(prediction)[1]
 
 
 
-    predictions = model.predict(BWImage)
-    prediction = np.argmax(predictions, axis=1)
+        return prediction
 
-    if prediction > 9:
-        prediction = str(chr(prediction + 55))
-    else:
-        prediction = str(prediction)
+    '''
+    
+    
+    '''
+    def returnDigits(self):
+        dir = './scans/'
+        path = os.path.join(dir)
+        plateNum = ""
+        i = 0
+        for img in os.listdir(path):
+            # print(returnPrediction("Scan" + str(i + 15) + ".png"))
+            plateNum += predictionMethods.returnPrediction(self, "digit0-" + str(i) + ".jpg")
+            i += 1
 
+        return plateNum
 
-    return prediction
+test = predictionMethods()
 
-for i in range(6):
-    print(returnPrediction("Scan" + str(i + 15) + ".png"))
-
+print(test.returnDigits())
 
 
 
